ba031b177c66d0985db3504daa88eb1f60a0ddf7
Leonardo Pereira <leonardopereira.silva@outlook.com>
2024-07-18 17:39:47 -0300

    replaced yad and anyrun by fuzzel

--- .local/share/scripts/wpselector.sh
+++ .local/share/scripts/wpselector.sh
@@ -4,4 +4,4 @@ SCRIPTSDIR=~/.local/share/scripts/
 WALLPAPERDIR=~/.local/share/wallpapers/
 
 cd $WALLPAPERDIR
-~/.local/share/scripts/chwp.sh "$(yad --width 900 --height 600 --file --add-preview --large-preview --title="Choose Wallpaper")"
+~/.local/share/scripts/chwp.sh "$WALLPAPERDIR/$(eza | fuzzel --dmenu)"
