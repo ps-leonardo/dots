08ae8fdf431028c3a15ac306cc8f52d0f50a478d
Leonardo Pereira <leonardopereira.silva@outlook.com>
2024-07-20 21:35:38 -0300

    fixed scrrecord.sh

--- .local/share/scripts/screenrecord.sh
+++ .local/share/scripts/screenrecord.sh
@@ -12,10 +12,10 @@ getactivemonitor() {
 
 cd ~/Videos || exit
 if pgrep wf-recorder >/dev/null; then
-  notify-send "Recording Stopped" "Stopped" -a 'record-script.sh' &
+  notify-send "Recording Stopped" "Stopped" -a 'screenrecord.sh' &
   pkill wf-recorder &
 else
-  notify-send "Starting recording" 'recording_'"$(getdate)"'.mp4' -a 'record-script.sh'
+  notify-send "Starting recording" 'recording_'"$(getdate)"'.mp4' -a 'screenrecord.sh'
   if [[ "$1" == "--sound" ]]; then
     wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --geometry "$(slurp)" --audio="$(getaudiooutput)" &
     disown
