0a183d377e26150f060212a4b50e5aa7b95f7a68
Leonardo Pereira <leonardopereira.silva@outlook.com>
2024-07-24 12:06:53 -0300

    improved install and added sddm theme

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/bash
 
 echo "Enabling parallel Downloads for Pacman and disable debug for makepkg"
+
 sudo cp /etc/pacman.conf /etc/pacman.conf.t2.bkp
 sudo sed -i "/^#Color/c\Color\nILoveCandy
 /^#VerbosePkgLists/c\VerbosePkgLists
 /^#ParallelDownloads/c\ParallelDownloads = 5" /etc/pacman.conf
 sudo cp /etc/makepkg.conf /etc/makepkg.conf.t2.bkp
 sudo sed -i "/^OPTIONS=(strip docs !libtool !staticlibs emptydirs zipman purge debug lto)/c\OPTIONS=(strip docs !libtool !staticlibs emptydirs zipman purge !debug lto)" /etc/makepkg.conf
+
+echo "\n"
+sleep 0.5
 
 # Check if yay or paru is installed and installs yay if none found.
 echo "Checking for aur helper"
@@ -18,24 +22,43 @@ if [ -z $aurhlpr ]; then
   aurhlpr=yay
 fi
 
+echo "\n"
+sleep 0.5
+
 set -euo pipefail
 
 # Install official and aur packages
+echo "Installing official and aur packages"
 sudo pacman -S --needed - <packages.lst
 $aurhlpr -S --needed - <aur.lst
 
+echo "\n"
+sleep 0.5
+
 # Copy configs, scripts and wallpapers
+echo "Copying configs, scripts and wallpapers"
 cp -r ~/Repos/dots/.config/* ~/.config/
 cp -r ~/Repos/dots/.local/share ~/.local/
 
-# Install tela circle dracula icon theme
-git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git ~/Repos/Tela-circle-icon-theme
-~/Repos/Tela-circle-icon-theme/install.sh -n 'Tela-circle-dracula-dark' -c
+echo "\n"
+sleep 0.5
 
 # Install LazyVim
+echo "Installing LazyVim"
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
+echo "\n"
+sleep 0.5
+
+# Install sddm theme and disable monitor 2 during login
+echo "Installing sddm theme sugar-candy and disabling monitor DP-2 during login"
+./sddm.sh
+
+echo "\n"
+sleep 0.5
+
 # Enable systemd services
+echo "Initializing system services"
 sudo systemctl enable bluetooth
 sudo systemctl enable sddm
