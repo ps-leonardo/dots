4ba90c03c5ba51e652f2a83952a343999569163b
Leonardo Periera <leonardopereira.silva@outlook.com>
2024-07-24 20:07:49 +0000

    added grub themes

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ sudo cp /etc/makepkg.conf /etc/makepkg.conf.t2.bkp
 sudo sed -i "/^OPTIONS=(strip docs !libtool !staticlibs emptydirs zipman purge debug lto)/c\OPTIONS=(strip docs !libtool !staticlibs emptydirs zipman purge !debug lto)" /etc/makepkg.conf
 
 echo ""
-sleep 0.5
 
 # Check if yay or paru is installed and installs yay if none found.
 echo "Checking for aur helper"
@@ -21,9 +20,9 @@ if [ -z $aurhlpr ]; then
   ./install_yay.sh
   aurhlpr=yay
 fi
+sleep 0.5
 
 echo ""
-sleep 0.5
 
 set -euo pipefail
 
@@ -31,42 +30,47 @@ set -euo pipefail
 echo "Installing official and aur packages"
 sudo pacman -S --needed - <packages.lst
 $aurhlpr -S --needed - <aur.lst
+sleep 0.5
 
 echo ""
-sleep 0.5
 
 # Install nerd fonts
 echo "Installing Nerd Fonts"
 sudo pacman -S --needed --asdeps nerd-fonts
 sudo pacman -S --needed --asdeps ttf-font-awesome
+sleep 0.5
 
 echo ""
-sleep 0.5
 
 # Copy configs, scripts and wallpapers
 echo "Copying configs, scripts and wallpapers"
 cp -r ~/Repos/dots/.config/* ~/.config/
 cp -r ~/Repos/dots/.local/share ~/.local/
+sleep 0.5
 
 echo ""
-sleep 0.5
 
 # Install LazyVim
 echo "Installing LazyVim"
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
+sleep 0.5
 
 echo ""
-sleep 0.5
 
 # Install sddm theme and disable monitor 2 during login
 echo "Installing sddm theme sugar-candy and disabling monitor DP-2 during login"
 ./sddm.sh
+sleep 0.5
 
 echo ""
-sleep 0.5
 
 # Enable systemd services
 echo "Initializing system services"
 sudo systemctl enable bluetooth
 sudo systemctl enable sddm
+sleep 0.5
+
+#Apply Grub themes
+echo "Applying grub theme"
+./apply_grub_themes.sh
