17d18da71281348469e9be3170831c2e832e0307
Leonardo Pereira <leonardopereira.silva@outlook.com>
2024-07-25 16:22:43 -0300

    fixes

---
+++ .local/share/scripts/duckstation_deps.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+sudo pacman -S --needed base-devel clang cmake curl extra-cmake-modules git alsa-lib dbus libdecor libglvnd libevdev fontconfig freetype2 gtk3 libgudev harfbuzz libinput libpipewire libpulse openssl systemd-libs wayland libx11 libxcb xcb-util-cursor xcb-util-wm xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util libxext libxkbcommon-x11 libxrandr lld llvm ninja pkgconf zlib
