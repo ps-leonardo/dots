7020be4a16cc25da14815dd2c83546a94f15d49a
Leonardo Pereira <leonardopereira.silva@outlook.com>
2024-07-26 11:17:08 -0300

    waybar

--- .local/share/scripts/chwp.sh
+++ .local/share/scripts/chwp.sh
@@ -10,9 +10,9 @@ if [[ -z $WALLPAPER ]]; then
   exit
 fi
 
-wallust run -s "$WALLPAPERDIR$WALLPAPER" >/dev/null 2>&1
+# wallust run -s "$WALLPAPERDIR$WALLPAPER" >/dev/null 2>&1
 
-pidof waybar >/dev/null && killall waybar
-waybar &
+#pidof waybar >/dev/null && killall waybar
+#waybar &
 
 swww img "$WALLPAPERDIR$WALLPAPER" --transition-type center
