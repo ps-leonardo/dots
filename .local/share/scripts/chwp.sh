b68a810b677363568b98c31aedce8711d2ee30bf
Leonardo Pereira <leonardopereira.silva@outlook.com>
2024-08-05 16:12:44 -0300

    changed wallpaper script to recursive look into directories

--- .local/share/scripts/chwp.sh
+++ .local/share/scripts/chwp.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/bash
 
-WALLPAPERDIR=~/.local/share/wallpapers/
+set -euo pipefail
 
+WALLPAPERDIR=~/.local/share/wallpapers/
 cd $WALLPAPERDIR
+
 WALLPAPER="$(eza | fuzzel --dmenu)"
 
 if [[ -z $WALLPAPER ]]; then
   notify-send 'No wallpaper selected'
   exit
 fi
+
+#TODO figure out a way to notify if nothing was selected in this part
+while [[ -d $WALLPAPER ]]; do
+  WALLPAPER="$WALLPAPER/"
+  WALLPAPERDIR="$WALLPAPERDIR$WALLPAPER"
+  cd "$WALLPAPERDIR"
+  WALLPAPER="$(eza | fuzzel --dmenu)"
+done
 
 # wallust run -s "$WALLPAPERDIR$WALLPAPER" >/dev/null 2>&1
 
@@ -17,4 +27,4 @@ fi
 
 swww img "$WALLPAPERDIR$WALLPAPER" --transition-type center
 
-magick "$WALLPAPERDIR$WALLPAPER" "current.jpg"
+magick "$WALLPAPERDIR$WALLPAPER" ~/.local/share/wallpapers/current.jpg
