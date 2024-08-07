bd7703b42c9cddea17edd8e6c5b154e2089935bb
Leonardo Pereira <leonardopereira.silva@outlook.com>
2024-08-07 16:00:06 -0300

    fixed copy_cfgs.sh

--- scripts/copy_cfgs.sh
+++ scripts/copy_cfgs.sh
@@ -6,8 +6,9 @@ THEME=$1
 
 if [[ $THEME == 'tokyo-night'  ]]; then
   $aurhlpr -S tokyonight-gtk-theme-git
-elif [[ $THEME == 'catppuccin-mocha ']]
+elif [[ $THEME == 'catppuccin-mocha' ]]; then
   $aurhlpr -S catppuccin-gtk-theme-mocha
+fi
 
 cp -r ~/Repos/dots/themes/$THEME/* ~/.config/
 cp -r ~/Repos/dots/.local/share ~/.local/
