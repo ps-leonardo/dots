08ef3111bba058cca3eed139248abccbd78973a8
Leonardo <leonardopereira.silva@outlook.com>
2024-08-23 23:37:25 -0300

    changed nvim config to lazyvim

--- themes/catppuccin-mocha/nvim/init.lua
+++ themes/catppuccin-mocha/nvim/init.lua
@@ -1,2 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("config.cmds")
+
+vim.filetype.add({
+  pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+})
---
+++ themes/catppuccin-mocha/nvim/lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ui = {
+      border = "single",
+      size = {
+        width = 1,
+        height = 1,
+      },
+    },
+  },
+}
