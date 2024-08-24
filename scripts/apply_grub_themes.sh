7b575249e74808e67eb3ce12a3442076733d6097
Leonardo Pereira <leonardopereira.silva@outlook.com>
2024-08-24 10:07:12 -0300

    enable os_prober in grub

--- scripts/apply_grub_themes.sh
+++ scripts/apply_grub_themes.sh
@@ -20,6 +20,7 @@ sudo sed -i "/^GRUB_DEFAULT=/c\GRUB_DEFAULT=saved
 /^GRUB_GFXMODE=/c\GRUB_GFXMODE=2560x1080x32,auto
 /^GRUB_THEME=/c\GRUB_THEME=\"/boot/grub/themes/$GRUBTHEME/theme.txt\"
 /^#GRUB_THEME=/c\GRUB_THEME=\"/boot/grub/themes/$GRUBTHEME/theme.txt\"
-/^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true" /etc/default/grub
+/^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true
+/^#GRUB_DISABLE_OS_PROBER=false/c\GRUB_DISABLE_OS_PROBER=false" /etc/default/grub
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
