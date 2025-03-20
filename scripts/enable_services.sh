bbef3c14ca5b165591ed5a1ec51c7e2857fb3619
Leonardo Pereira <leonardopereira.silva@outlook.com>
2025-03-20 11:37:25 -0300

    added reflector service

--- scripts/enable_services.sh
+++ scripts/enable_services.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
-#Enable systemd services
+# Copy Reflector config
+sudo cp ~/Repos/dots/optionals/reflector/reflector.conf /etc/xdg/reflector/reflector.conf
+
+# Enable systemd services
 sudo systemctl enable bluetooth
 sudo systemctl enable sddm
+sudo systemctl enable reflector
