73e394e81dc20851242ba43e73ba5d2f22b31518
Leonardo Pereira <leonardopereira.silva@outlook.com>
2025-09-26 11:17:26 -0300

    fixed scripts move

--- .config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
-vim.filetype.add({
-  pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
-})
--- scripts/copy_cfgs.sh
+++ scripts/copy_cfgs.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 cp -r ~/Repos/dots/.config/* ~/.config/
-cp -r ~/Repos/dots/.local/share ~/.local/
+cp -r ~/Repos/dots/.local/share ~/.local/share
